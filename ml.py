7daae4ff0c10e027d9dc59344e9c88c467efadf9
Karan Shukla <shuklak13@users.noreply.github.com>
2017-04-22 23:09:26 -0500

    Cross-validation

--- ml.py
+++ ml.py
@@ -22,6 +22,7 @@ from pyspark.mllib.linalg import Vectors
 from pyspark.ml.regression import LinearRegression, DecisionTreeRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
 from kafka import KafkaConsumer
+from random import shuffle
 import os
 
 
@@ -39,26 +40,30 @@ def kafkaStream(topic, group = KAFKA_GROUP, port = KAFKA_PORT):
 			group_id=group,
 			bootstrap_servers=[port])
 
-# returns data as a List of (y, x) Tuples, where x is a Vector
+# returns a List of (y, x) Tuples, where x is a Vector
 def getFromTopic(topic):
-	Actual Data streamed from Kafka:
+	# Actual Data streamed from Kafka:
 	data = []
 	for message in kafkaStream(topic=topic):
 		if message.key == "END":
-	 		break
-	 	y = message.key
-	 	x = Vectors.dense(message.value.split(" "))
-	 	data_point = (y, x)
-	 	data.extend(data_point)
+			break
+		y = message.key
+		x = Vectors.dense(message.value.split(" "))
+		data_point = (y, x)
+		data.extend(data_point)
 
 	# Sample Data: use for testing purposes
-	#data = [
-	#(1.0, Vectors.dense(2.0)),
-	#(1.0, Vectors.dense(3.0)),
-	#(1.0, Vectors.dense(4.0)),
-	#(2.0, Vectors.dense(5.0)),
-	#]
-	
+	# data = [
+	# (1.0, Vectors.dense(2.0)),
+	# (9.0, Vectors.dense(3.0)),
+	# (3.0, Vectors.dense(4.0)),
+	# (2.0, Vectors.dense(5.0)),
+	# (5.0, Vectors.dense(6.0)),
+	# (4.0, Vectors.dense(1.0)),
+	# (7.0, Vectors.dense(4.5)),
+	# (8.0, Vectors.dense(5.5)),
+	# ]
+
 	return data
 
 def toDataFrame(data):
@@ -70,9 +75,48 @@ def trainLRmodel(training_df):
 def trainDTmodel(training_df):
 	return DecisionTreeRegressor(featuresCol="x", labelCol="y", predictionCol="prediction").fit(training_df)
 
-def test(lr_model, dt_model, testing_df):
-	print "The Root-Mean-Square-Error of the Linear Regression Model is " + str(root_mean_square_error(lr_model, testing_df))
-	print "The Root-Mean-Square-Error of the Decision Tree Model is " + str(root_mean_square_error(dt_model, testing_df))
+def test_single_split(model):
+	(training_df, testing_df) = data.randomSplit([0.75, 0.25])
+	lr_model = trainLRmodel(training_df)
+	dt_model = trainDTmodel(training_df)
+	print "The Root-Mean-Square-Error of the Linear Regression Model is " + str(root_mean_square_error(model, testing_df))
+	print "The Root-Mean-Square-Error of the Decision Tree Model is " + str(root_mean_square_error(model, testing_df))
+
+def createPartitions(df, numPartitions=3):
+	rows = df.collect()
+	shuffle(df.collect())
+	numRows = len(rows)
+	partitionSize = numRows / numPartitions
+	partitions = []
+	for i in range(numPartitions):
+		start = i*partitionSize
+		end = (i+1)*partitionSize
+		partition = toDataFrame(rows[start:end])
+		partitions.append(partition)
+	return partitions
+
+def mergeTrainingPartitions(partitions, testing_df):
+	training_df = []
+	for partition in partitions:
+		if partition is not testing_df:
+			if not training_df:
+				training_df = partition
+			else:
+				training_df.unionAll(partition)
+	return training_df
+
+def test_cross_validation(data, n=3):
+	partitions = createPartitions(data, n)
+	lr_rmse = []
+	dt_rmse = []
+	for testing_df in partitions:
+		training_df = mergeTrainingPartitions(partitions, testing_df)
+		lr_model = trainLRmodel(training_df)
+		dt_model = trainDTmodel(training_df)
+		lr_rmse.append(root_mean_square_error(lr_model, testing_df))
+		dt_rmse.append(root_mean_square_error(dt_model, testing_df))
+	print "The Root-Mean-Square-Errors of the Linear Regression Models are " + " ".join(map(str, lr_rmse))
+	print "The Root-Mean-Square-Errors of the Decision Tree Models are " + " ".join(map(str, dt_rmse))
 
 def root_mean_square_error(model, testing_df):
 	predictions = model.transform(testing_df)
@@ -84,7 +128,4 @@ def root_mean_square_error(model, testing_df):
 #	MAIN METHOD	#
 if __name__ == "__main__":
 	data = toDataFrame(getFromTopic(KAFKA_TOPIC))
-	(training_df, testing_df) = data.randomSplit([0.75,0.25])
-	lr_model = trainLRmodel(training_df)
-	dt_model = trainDTmodel(training_df)
-	test(lr_model, dt_model, testing_df)
+	test_cross_validation(data)
